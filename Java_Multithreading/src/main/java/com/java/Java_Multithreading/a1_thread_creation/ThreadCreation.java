254a66637faa1f26bdaccfad608d8915dae96be4
prosenjit005 <33807029+prosenjit05@users.noreply.github.com>
2023-10-24 20:12:47 +0530

    Update ThreadCreation.java

--- Java_Multithreading/src/main/java/com/java/Java_Multithreading/a1_thread_creation/ThreadCreation.java
+++ Java_Multithreading/src/main/java/com/java/Java_Multithreading/a1_thread_creation/ThreadCreation.java
@@ -8,8 +8,12 @@ public class ThreadCreation {
             public void run() {
                 //Code that will run in  a new thread
                 System.out.println("We are now in thread: " + Thread.currentThread().getName());
+                System.out.println("Current thread priority is: " + Thread.currentThread().getPriority());
             }
         });
+
+        thread.setName("New Worker Thread");
+        thread.setPriority(Thread.MAX_PRIORITY);
 
         System.out.println("We are in thread: \"" + Thread.currentThread().getName() + "\" before starting a new thread");
         thread.start();
