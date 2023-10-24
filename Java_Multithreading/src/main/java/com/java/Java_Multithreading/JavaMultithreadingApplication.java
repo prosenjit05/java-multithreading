15b0766c8b0155c33fd88f1022fde032805e47a8
prosenjit005 <33807029+prosenjit05@users.noreply.github.com>
2023-10-24 19:23:03 +0530

    initial commit

---
+++ Java_Multithreading/src/main/java/com/java/Java_Multithreading/JavaMultithreadingApplication.java
@@ -0,0 +1,13 @@
+package com.java.Java_Multithreading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMultithreadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMultithreadingApplication.class, args);
+	}
+
+}
---
+++ Java_Multithreading/src/test/java/com/java/Java_Multithreading/JavaMultithreadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.Java_Multithreading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMultithreadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
