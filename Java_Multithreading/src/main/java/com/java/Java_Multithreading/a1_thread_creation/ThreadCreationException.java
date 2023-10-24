81662c4613488991fc54ed9d2d1355eba806e69b
prosenjit005 <33807029+prosenjit05@users.noreply.github.com>
2023-10-24 20:37:01 +0530

    Create ThreadCreationException.java

---
+++ Java_Multithreading/src/main/java/com/java/Java_Multithreading/a1_thread_creation/ThreadCreationException.java
@@ -0,0 +1,27 @@
+package com.java.Java_Multithreading.a1_thread_creation;
+
+public class ThreadCreationException {
+    public static void main(String[] args) {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //Code that will run in a new thread
+                throw new RuntimeException("Intentional Exception");
+            }
+        });
+
+        thread.setName("Misbehaving thread");
+
+        thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println("A critical error happened in thread " + t.getName()
+                        + " the error is " + e.getMessage());
+            }
+        });
+
+        thread.start();
+
+    }
+}
