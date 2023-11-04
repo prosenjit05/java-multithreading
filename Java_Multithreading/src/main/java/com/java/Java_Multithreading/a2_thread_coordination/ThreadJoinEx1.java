c844a9982677546389ff13db30925b4a86b3ca09
prosenjit005 <33807029+prosenjit05@users.noreply.github.com>
2023-11-04 13:34:20 +0530

    Create ThreadJoinEx1.java

---
+++ Java_Multithreading/src/main/java/com/java/Java_Multithreading/a2_thread_coordination/ThreadJoinEx1.java
@@ -0,0 +1,13 @@
+package com.java.Java_Multithreading.a2_thread_coordination;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreadJoinEx1 {
+
+    public static void main(String[] args) {
+        List<Long> inputNumbers = Arrays.asList(100000000L, 3435L, 35435L, 2324L, 4656L, 23L, 5556L);
+
+    }
+
+}
