57f98a54ec29616a6739c672736ef406686287a8
prosenjit005 <33807029+prosenjit05@users.noreply.github.com>
2023-11-05 09:48:48 +0530

    Create LatencyOptimisation.java

---
+++ Java_Multithreading/src/main/java/com/java/Java_Multithreading/a3_performance_optimisation/LatencyOptimisation.java
@@ -0,0 +1,17 @@
+package com.java.Java_Multithreading.a3_performance_optimisation;
+
+public class LatencyOptimisation {
+
+    public static int getRed(int rgb) {
+        return (rgb & 0x00FF0000) >> 16;
+    }
+
+    public static int getGreen(int rgb) {
+        return (rgb & 0x0000FF00) >> 8;
+    }
+
+    public static int getBlue(int rgb) {
+        return rgb & 0x000000FF;
+    }
+
+}
