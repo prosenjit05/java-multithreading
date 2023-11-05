d57a5bf11b2199de732df0a06114fb70156d2fd3
prosenjit005 <33807029+prosenjit05@users.noreply.github.com>
2023-11-05 09:34:58 +0530

    Update ThreadJoinEx1.java

--- Java_Multithreading/src/main/java/com/java/Java_Multithreading/a2_thread_coordination/ThreadJoinEx1.java
+++ Java_Multithreading/src/main/java/com/java/Java_Multithreading/a2_thread_coordination/ThreadJoinEx1.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class ThreadJoinEx1 {
 
     public static void main(String[] args) throws InterruptedException {
-        List<Long> inputNumbers = Arrays.asList(3435L, 35435L, 2324L, 4656L, 23L, 5556L);
+        List<Long> inputNumbers = Arrays.asList(100000000L, 3435L, 35435L, 2324L, 4656L, 23L, 5556L);
 
         List<FactorialThread> threads = new ArrayList<>();
 
@@ -17,11 +17,12 @@ public class ThreadJoinEx1 {
         }
 
         for (Thread thread : threads) {
+            thread.setDaemon(true);
             thread.start();
         }
 
         for (Thread thread : threads) {
-            thread.join();
+            thread.join(2000);
         }
 
         for (int i = 0; i < inputNumbers.size(); i++) {
