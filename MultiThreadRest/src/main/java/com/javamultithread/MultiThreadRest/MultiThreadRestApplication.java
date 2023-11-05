ea06c3d7082eda14b0897a68c682eb8f5be20ff5
prosenjit005 <33807029+prosenjit05@users.noreply.github.com>
2023-11-05 21:04:10 +0530

    initial commit

---
+++ MultiThreadRest/src/main/java/com/javamultithread/MultiThreadRest/MultiThreadRestApplication.java
@@ -0,0 +1,13 @@
+package com.javamultithread.MultiThreadRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiThreadRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiThreadRestApplication.class, args);
+	}
+
+}
---
+++ MultiThreadRest/src/test/java/com/javamultithread/MultiThreadRest/MultiThreadRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javamultithread.MultiThreadRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiThreadRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
