b77890e9cb83cc9e18bf1d09c8a1626737b74abf
prosenjit005 <33807029+prosenjit05@users.noreply.github.com>
2023-11-07 20:19:23 +0530

    Create ApplicationMetrics.java

---
+++ Java_Multithreading/src/main/java/com/java/Java_Multithreading/a5_concurrency/ApplicationMetrics.java
@@ -0,0 +1,15 @@
+package com.java.Java_Multithreading.a5_concurrency;
+
+public class ApplicationMetrics {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class Metrics {
+        private long count = 0;
+        private double average = 0.0;
+    }
+
+
+}
