60183a469b044f1f80728f079a1285a9bceb9d4e
prosenjit005 <33807029+prosenjit05@users.noreply.github.com>
2023-11-07 17:14:32 +0530

    Update DataRacesExample1.java

--- Java_Multithreading/src/main/java/com/java/Java_Multithreading/a4_memory_regions/DataRacesExample1.java
+++ Java_Multithreading/src/main/java/com/java/Java_Multithreading/a4_memory_regions/DataRacesExample1.java
@@ -2,14 +2,54 @@ package com.java.Java_Multithreading.a4_memory_regions;
 
 public class DataRacesExample1 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        InventoryCounter inventoryCounter = new InventoryCounter();
+        IncrementingThread incrementingThread = new IncrementingThread(inventoryCounter);
+        DecrementingThread decrementingThread = new DecrementingThread(inventoryCounter);
 
+        incrementingThread.start();
+        incrementingThread.join();
+
+        decrementingThread.start();
+        decrementingThread.join();
+
+        System.out.println("We currently have " + inventoryCounter.getItems() + " items");
+    }
+
+    public static class DecrementingThread extends Thread {
+
+        private InventoryCounter inventoryCounter;
+
+        public DecrementingThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                inventoryCounter.decrement();
+            }
+        }
+    }
+
+    public static class IncrementingThread extends Thread {
+
+        private InventoryCounter inventoryCounter;
+
+        public IncrementingThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                inventoryCounter.increment();
+            }
+        }
     }
 
     private static class InventoryCounter {
         private int items = 0;
-
-        Object lock = new Object();
 
         public void increment() {
             items++;
