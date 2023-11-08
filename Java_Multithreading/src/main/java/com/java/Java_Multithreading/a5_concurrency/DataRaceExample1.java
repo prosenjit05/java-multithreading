33c38c3826211c8af2e4096f900f8c1e8a121097
prosenjit005 <33807029+prosenjit05@users.noreply.github.com>
2023-11-08 10:20:27 +0530

    Update DataRaceExample1.java

--- Java_Multithreading/src/main/java/com/java/Java_Multithreading/a5_concurrency/DataRaceExample1.java
+++ Java_Multithreading/src/main/java/com/java/Java_Multithreading/a5_concurrency/DataRaceExample1.java
@@ -3,7 +3,22 @@ package com.java.Java_Multithreading.a5_concurrency;
 public class DataRaceExample1 {
 
     public static void main(String[] args) {
+        SharedClass sharedClass = new SharedClass();
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < Integer.MAX_VALUE; i++) {
+                sharedClass.increment();
+            }
+        });
 
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < Integer.MAX_VALUE; i++) {
+                sharedClass.checkForDataRace();
+            }
+
+        });
+
+        thread1.start();
+        thread2.start();
     }
 
     public static class SharedClass {
