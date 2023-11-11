0ef740acfee83b376d59e83547091f1b0f321d9f
prosenjit005 <33807029+prosenjit05@users.noreply.github.com>
2023-11-11 17:16:16 +0530

    backpressureexample1

--- Java_Multithreading/src/main/java/com/java/Java_Multithreading/a7_inter_thread_communication/BackpressureExample1a.java
+++ Java_Multithreading/src/main/java/com/java/Java_Multithreading/a7_inter_thread_communication/BackpressureExample1a.java
@@ -1,13 +1,130 @@
 package com.java.Java_Multithreading.a7_inter_thread_communication;
 
 
+import java.io.*;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
+import java.util.StringJoiner;
 
 public class BackpressureExample1a {
+    private static final String INPUT_FILE = "./output/matrices";
+    private static final String OUTPUT_FILE = "./output/matrices_results.txt";
+    private static final int N = 10;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        ThreadSafeQueue threadSafeQueue = new ThreadSafeQueue();
+        File inputFile = new File(INPUT_FILE);
+        File outputFile = new File(OUTPUT_FILE);
 
+        MatricesReaderProducer matricesReader = new MatricesReaderProducer(new FileReader(inputFile), threadSafeQueue);
+        MatricesMultiplierConsumer matricesConsumer = new MatricesMultiplierConsumer(new FileWriter(outputFile), threadSafeQueue);
+
+        matricesConsumer.start();
+        matricesReader.start();
+    }
+
+    private static class MatricesMultiplierConsumer extends Thread {
+        private ThreadSafeQueue queue;
+        private FileWriter fileWriter;
+
+        public MatricesMultiplierConsumer(FileWriter fileWriter, ThreadSafeQueue queue) {
+            this.fileWriter = fileWriter;
+            this.queue = queue;
+        }
+
+        private static void saveMatrixToFile(FileWriter fileWriter, float[][] matrix) throws IOException {
+            for (int r = 0; r < N; r++) {
+                StringJoiner stringJoiner = new StringJoiner(", ");
+                for (int c = 0; c < N; c++) {
+                    stringJoiner.add(String.format("%.2f", matrix[r][c]));
+                }
+                fileWriter.write(stringJoiner.toString());
+                fileWriter.write('\n');
+            }
+            fileWriter.write('\n');
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                MatricesPair matricesPair = queue.remove();
+                if (matricesPair == null) {
+                    System.out.println("No more matrices to read from the queue, consumer is terminating");
+                    break;
+                }
+
+                float[][] result = multiplyMatrices(matricesPair.matrix1, matricesPair.matrix2);
+
+                try {
+                    saveMatrixToFile(fileWriter, result);
+                } catch (IOException e) {
+                }
+            }
+
+            try {
+                fileWriter.flush();
+                fileWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private float[][] multiplyMatrices(float[][] m1, float[][] m2) {
+            float[][] result = new float[N][N];
+            for (int r = 0; r < N; r++) {
+                for (int c = 0; c < N; c++) {
+                    for (int k = 0; k < N; k++) {
+                        result[r][c] += m1[r][k] * m2[k][c];
+                    }
+                }
+            }
+            return result;
+        }
+    }
+
+    private static class MatricesReaderProducer extends Thread {
+        private Scanner scanner;
+        private ThreadSafeQueue queue;
+
+        public MatricesReaderProducer(FileReader reader, ThreadSafeQueue queue) {
+            this.scanner = new Scanner(reader);
+            this.queue = queue;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                float[][] matrix1 = readMatrix();
+                float[][] matrix2 = readMatrix();
+                if (matrix1 == null || matrix2 == null) {
+                    queue.terminate();
+                    System.out.println("No more matrices to read. Producer Thread is terminating");
+                    return;
+                }
+
+                MatricesPair matricesPair = new MatricesPair();
+                matricesPair.matrix1 = matrix1;
+                matricesPair.matrix2 = matrix2;
+
+                queue.add(matricesPair);
+            }
+        }
+
+        private float[][] readMatrix() {
+            float[][] matrix = new float[N][N];
+            for (int r = 0; r < N; r++) {
+                if (!scanner.hasNext()) {
+                    return null;
+                }
+                String[] line = scanner.nextLine().split(",");
+                for (int c = 0; c < N; c++) {
+                    matrix[r][c] = Float.valueOf(line[c]);
+                }
+            }
+            scanner.nextLine();
+            return matrix;
+        }
     }
 
     private static class ThreadSafeQueue {
@@ -43,7 +160,8 @@ public class BackpressureExample1a {
         }
 
         public synchronized void terminate() {
-
+            isTerminate = true;
+            notifyAll();
         }
 
     }
