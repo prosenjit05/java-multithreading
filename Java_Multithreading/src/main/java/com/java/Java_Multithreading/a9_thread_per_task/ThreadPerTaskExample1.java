782bf29c69ab673eb5fb885b45b92e2fbffcb49b
prosenjit005 <33807029+prosenjit05@users.noreply.github.com>
2023-11-11 20:14:44 +0530

    Update ThreadPerTaskExample1.java

--- Java_Multithreading/src/main/java/com/java/Java_Multithreading/a9_thread_per_task/ThreadPerTaskExample1.java
+++ Java_Multithreading/src/main/java/com/java/Java_Multithreading/a9_thread_per_task/ThreadPerTaskExample1.java
@@ -1,9 +1,36 @@
 package com.java.Java_Multithreading.a9_thread_per_task;
 
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class ThreadPerTaskExample1 {
+    private static final int NUMBER_OF_TASKS = 1000;
 
     public static void main(String[] args) {
-        
+        System.out.printf("Running %d tasks\n", NUMBER_OF_TASKS);
+        long start = System.currentTimeMillis();
+        performTasks();
+        System.out.printf("Tasks took %dms to complete\n", System.currentTimeMillis() - start);
+    }
+
+    private static void performTasks() {
+        try (ExecutorService executorService = Executors.newCachedThreadPool()) {
+
+            for (int i = 0; i < NUMBER_OF_TASKS; i++) {
+                executorService.submit(() ->  blockingIoOperation());
+            }
+        }
+    }
+
+    // Simulates a long blocking IO
+    private static void blockingIoOperation() {
+        System.out.println("Executing a blocking task from thread: " + Thread.currentThread());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
