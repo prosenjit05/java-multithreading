8114199dbeda72b7cb6663aac53bdb9e404c03e2
prosenjit005 <33807029+prosenjit05@users.noreply.github.com>
2023-11-11 21:27:35 +0530

    Update VirtualThreadExample1.java

--- Java_Multithreading/src/main/java/com/java/Java_Multithreading/b10_virtual_thread/VirtualThreadExample1.java
+++ Java_Multithreading/src/main/java/com/java/Java_Multithreading/b10_virtual_thread/VirtualThreadExample1.java
@@ -2,8 +2,12 @@ package com.java.Java_Multithreading.b10_virtual_thread;
 
 public class VirtualThreadExample1 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        Runnable runnable = () -> System.out.println("Inside thread: " + Thread.currentThread());
+        Thread platformThread = new Thread(runnable);
 
+        platformThread.start();
+        platformThread.join();
     }
 
 }
