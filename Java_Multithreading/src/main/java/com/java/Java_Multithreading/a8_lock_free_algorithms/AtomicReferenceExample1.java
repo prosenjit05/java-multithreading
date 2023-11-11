bfcc69dcdd69243bf7e2b9126ded11c78ec2d84c
prosenjit005 <33807029+prosenjit05@users.noreply.github.com>
2023-11-11 19:11:19 +0530

    Update AtomicReferenceExample1.java

--- Java_Multithreading/src/main/java/com/java/Java_Multithreading/a8_lock_free_algorithms/AtomicReferenceExample1.java
+++ Java_Multithreading/src/main/java/com/java/Java_Multithreading/a8_lock_free_algorithms/AtomicReferenceExample1.java
@@ -1,9 +1,20 @@
 package com.java.Java_Multithreading.a8_lock_free_algorithms;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 public class AtomicReferenceExample1 {
 
     public static void main(String[] args) {
-        
+        String oldName = "old name";
+        String newName = "new name";
+        AtomicReference<String> atomicReference = new AtomicReference<>(oldName);
+
+        atomicReference.set("unexpected name");
+        if (atomicReference.compareAndSet(oldName, newName)) {
+            System.out.println("New value is " + atomicReference.get());
+        } else {
+            System.out.println("Nothing Changed");
+        }
     }
 
 }
