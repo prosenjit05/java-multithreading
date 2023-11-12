ec43e147734207f46f2b6490e37e8e21042d1109
prosenjit005 <33807029+prosenjit05@users.noreply.github.com>
2023-11-12 12:56:37 +0530

    1b

---
+++ Java_Multithreading/src/main/java/com/java/Java_Multithreading/b10_virtual_thread/VirtualThreadExample1b.java
@@ -0,0 +1,28 @@
+package com.java.Java_Multithreading.b10_virtual_thread;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VirtualThreadExample1b {
+    private static final int NUMBER_OF_VIRTUAL_THREADS = 2;
+
+    public static void main(String[] args) throws InterruptedException {
+        Runnable runnable = () -> System.out.println("Inside thread: " + Thread.currentThread());
+
+        List<Thread> virtualThreads = new ArrayList<>();
+
+        for (int i = 0; i < NUMBER_OF_VIRTUAL_THREADS; i++) {
+            Thread virtualThread = Thread.ofVirtual().unstarted(runnable);
+            virtualThreads.add(virtualThread);
+        }
+
+        for (Thread virtualThread : virtualThreads) {
+            virtualThread.start();
+        }
+
+        for (Thread virtualThread : virtualThreads) {
+            virtualThread.join();
+        }
+    }
+
+}
